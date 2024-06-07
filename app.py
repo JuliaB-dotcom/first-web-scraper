658ba16f69dc0e19da854102706ceadc9aa01754
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2024-06-07 22:45:54 +0200

    Adding data into the table

--- app.py
+++ app.py
@@ -12,18 +12,18 @@ def home():
         ica_maxi_mince = requests.get('https://handlaprivatkund.ica.se/stores/1004219/search?q=blandf%C3%A4rs&sortBy=pricePerDescending')
         soup_mince = BeautifulSoup(ica_maxi_mince.text, 'lxml')
         mince_name = soup_mince.find('h3', class_ = 'text__Text-sc-6l1yjp-0 iWlLMY').text
-        mince_price = soup_mince.find('div', class_ = 'base__Price-sc-1mnb0pd-29 sc-dJjZJu gsTqdV gShugV').text
-        mince_info =  mince_name + '' + mince_price
+        mince_price = soup_mince.find('div', class_ = 'base__PriceWrapper-sc-1mnb0pd-28 dDLLyP').text
+    
     
     except requests.RequestException as e:
     # Handle request exceptions
-        mince_price = f"Error fetching data: {e}"
+        mince_info = f"Error fetching data: {e}"
     
     except Exception as e:
     # Handle all other exceptions
-        mince_price= f"An error occurred: {e}"
+        mince_info= f"An error occurred: {e}"
 
-    return render_template("index.html", mince_price=mince_price)
+    return render_template("index.html", mince_name=mince_name, mince_price = mince_price)
 
 # Runs the app
 if __name__ == '__main__':
