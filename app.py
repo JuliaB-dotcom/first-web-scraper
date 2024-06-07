ba2ff5744b596c6a97b4913b702e017c32b158df
JuliB-dotcom <69867011+JuliaB-dotcom@users.noreply.github.com>
2024-06-07 23:23:02 +0200

    Adding ica maxi data

--- app.py
+++ app.py
@@ -8,22 +8,32 @@ app = Flask(__name__)
 # Adds route
 @app.route('/')
 def home():
-    try:
+    
         ica_maxi_mince = requests.get('https://handlaprivatkund.ica.se/stores/1004219/search?q=blandf%C3%A4rs&sortBy=pricePerDescending')
-        soup_mince = BeautifulSoup(ica_maxi_mince.text, 'lxml')
-        mince_name = soup_mince.find('h3', class_ = 'text__Text-sc-6l1yjp-0 iWlLMY').text
-        mince_price = soup_mince.find('div', class_ = 'base__PriceWrapper-sc-1mnb0pd-28 dDLLyP').text
-    
-    
-    except requests.RequestException as e:
-    # Handle request exceptions
-        mince_info = f"Error fetching data: {e}"
-    
-    except Exception as e:
-    # Handle all other exceptions
-        mince_info= f"An error occurred: {e}"
+        soup_maxi_mince = BeautifulSoup(ica_maxi_mince.text, 'lxml')
+        mince_maxi_name = soup_maxi_mince.find('h3', class_ = 'text__Text-sc-6l1yjp-0 iWlLMY').text
+        mince_maxi_price = soup_maxi_mince.find('div', class_ = 'base__PriceWrapper-sc-1mnb0pd-28 dDLLyP').text
 
-    return render_template("index.html", mince_name=mince_name, mince_price = mince_price)
+        ica_maxi_cheese = requests.get('https://handlaprivatkund.ica.se/stores/1004219/search?q=ost&sortBy=pricePerDescending')
+        soup_maxi_cheese = BeautifulSoup(ica_maxi_cheese.text, 'lxml')
+        cheese_maxi_name = soup_maxi_cheese.find('h3', class_ = 'text__Text-sc-6l1yjp-0 iWlLMY').text
+        cheese_maxi_price = soup_maxi_cheese.find('div', class_ = 'base__PriceWrapper-sc-1mnb0pd-28 dDLLyP').text
+
+        ica_maxi_fish = requests.get('https://handlaprivatkund.ica.se/stores/1004219/search?q=fisk&sortBy=pricePerDescending')
+        soup_maxi_fish = BeautifulSoup(ica_maxi_fish.text, 'lxml')
+        fish_maxi_name = soup_maxi_fish.find('h3', class_ = 'text__Text-sc-6l1yjp-0 iWlLMY').text
+        fish_maxi_price = soup_maxi_fish.find('div', class_ = 'base__PriceWrapper-sc-1mnb0pd-28 dDLLyP').text
+
+        ica_maxi_potato = requests.get('https://handlaprivatkund.ica.se/stores/1004219/search?q=Fast%20Potatis&sortBy=pricePerDescending')
+        soup_maxi_potato = BeautifulSoup(ica_maxi_potato.text, 'lxml')
+        potato_maxi_name = soup_maxi_potato.find('h3', class_ = 'text__Text-sc-6l1yjp-0 iWlLMY').text
+        potato_maxi_price = soup_maxi_potato.find('div', class_ = 'base__PriceWrapper-sc-1mnb0pd-28 dDLLyP').text
+    
+
+
+        return render_template("index.html", mince_maxi_name=mince_maxi_name, mince_maxi_price=mince_maxi_price, cheese_maxi_name=cheese_maxi_name,
+                                cheese_maxi_price=cheese_maxi_price, fish_maxi_name=fish_maxi_name, fish_maxi_price=fish_maxi_price,
+                                  potato_maxi_name=potato_maxi_name, potato_maxi_price=potato_maxi_price)
 
 # Runs the app
 if __name__ == '__main__':
